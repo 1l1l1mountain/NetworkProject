890d8025290ad078ec57b273a3244e0a44e6db44
(윤하운) <(un7528@naver.com)>
2024-11-04 17:56:36 +0900

    찐막

--- src/DeleteUI.java
+++ src/DeleteUI.java
@@ -3,44 +3,39 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class DeleteUI extends JFrame{
+public class DeleteUI extends JFrame {
   public JTextField fileNameField;
-  
-  public DeleteUI(){
-  setTitle("파일 삭제");
-  setSize(600, 300);
-  setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-  setLayout(new FlowLayout());
 
-  
-  fileNameField = new JTextField(20); 
-  add(new JLabel("삭제할 파일 이름:")); 
-  add(fileNameField); 
+  public DeleteUI() {
+    setTitle("파일 삭제");
+    setSize(600, 300);
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    setLayout(new FlowLayout());
 
-  JButton deleteButton = new JButton("삭제");
-  deleteButton.addActionListener(new DeleteButtonListener()); // 클릭 이벤트 핸들러 추가
-  add(deleteButton);
-}
+    fileNameField = new JTextField(20);
+    add(new JLabel("삭제할 파일 이름:"));
+    add(fileNameField);
 
-
-        
-
-public class DeleteButtonListener implements ActionListener {
-
-  @Override
-  public void actionPerformed(ActionEvent e) { 
-    try{
-      NetStream netstream = new NetStream();
-      String fileName = fileNameField.getText().trim(); // 텍스트 필드에서 파일 이름 가져오기
-      netstream.SendCommand("DELE " + fileName);
-      System.out.println(netstream.ReceiveResponse());
-        
-      dispose();
-    }
-    catch(Exception e1){
-    }
-    
+    JButton deleteButton = new JButton("삭제");
+    deleteButton.addActionListener(new DeleteButtonListener()); // 클릭 이벤트 핸들러 추가
+    add(deleteButton);
   }
-}
+
+  public class DeleteButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      try {
+        String fileName = fileNameField.getText().trim(); // 텍스트 필드에서 파일 이름 가져오기
+        NetStream.SendCommand("DELE " + fileName);
+        System.out.println(NetStream.ReceiveResponse());
+
+        dispose();
+      } catch (Exception e1) {
+        e1.printStackTrace();
+      }
+
+    }
+  }
 
 }
