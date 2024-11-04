e3e336b9e6d84f31be00648615e740632bc810cb
(윤하운) <(un7528@naver.com)>
2024-11-04 20:37:46 +0900

    asd

--- src/DeleteUI.java
+++ src/DeleteUI.java
@@ -1,42 +1,25 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.io.IOException;
 
-public class DeleteUI extends JFrame {
-    public JTextField fileNameField;
+public class DeleteUI {
+  public DeleteUI(){
 
-    public DeleteUI() {
-        setTitle("서버에 있는 파일 삭제");
-        setSize(600, 300);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setLayout(new FlowLayout());
+  }
+    
 
-        fileNameField = new JTextField(20);
-        add(new JLabel("삭제할 파일 이름:"));
-        add(fileNameField);
+    public void Do(String fileName) {
+      
+      try {
+        NetStream.SendCommand("DELE " + fileName);
 
-        JButton deleteButton = new JButton("삭제");
-        // 버튼에 클릭 이벤트 핸들러 추가
-        deleteButton.addActionListener(new DeleteButtonListener()); 
-        add(deleteButton);
+        String response = NetStream.ReceiveResponse();
+            // 서버 응답 출력
+          CommandUI.outputArea.append("서버 응답: " + response + "\n");
+            
+      } catch (IOException e) {
+        CommandUI.outputArea.append("오류 발생: " + e.getMessage()+"\n");
+      }  
+
     }
 
-    public class DeleteButtonListener implements ActionListener {
-        @Override
-        //버튼 클릭시 발생하는 이벤트
-        public void actionPerformed(ActionEvent e) {
-            try {
-                // 텍스트 필드에서 삭제할 파일 이름 가져오기
-                String fileName = fileNameField.getText().trim(); 
-                // 파일 삭제 명령을 서버에 전송
-                NetStream.SendCommand("DELE " + fileName);
-                //서버 응답 출력
-                System.out.println(NetStream.ReceiveResponse());
-                dispose();
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        }
+    
     }
-}
